47689ab5f3ea1eb78934e2ad5783e631d29f74c8
park <hollyzero@naver.com>
2020-04-02 13:20:04 +0900

    1st commit!

---
+++ Lotoofm.py
@@ -0,0 +1,22 @@
+import random
+
+def makeNums():
+    balls = [i for i in range(1,46)]
+    random.shuffle(balls)
+    return balls[0:6]
+def input_display():
+    money = int(input('금액을 입력하시오'))
+    if money % 1000 != 0:
+        print('try again')
+        return input_display()
+    else:
+        return int(money / 1000)
+
+def main():
+    count = input_display()
+    for x in range(int(count)):
+        nums = makeNums()
+        print(sorted(nums))
+    print('---------------------------')
+if __name__ == "__main__":
+    main()
